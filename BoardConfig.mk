982028e11f596e22fc60ebd853993c0f3866bf84
me-cafebabe <me.cafebabe@gmail.com>
2023-02-11 06:34:49 +0800

    pyxis: Add support for MI CC 9 Meitu Edition (vela)
    * vela is mostly just a copy of pyxis, with some camera mods from Meitu in addition.
    * pyxis camera stack supports all camera sensors of vela, but vela camera stack doesn't support all pyxis ones
    * People have been using pyxis builds on vela directly for long
    
    Change-Id: I630daaada280ff51e1d18347c80e0725c675ec9d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,6 +8,7 @@
 include device/xiaomi/sdm710-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/pyxis
+USES_DEVICE_XIAOMI_PYXIS := true
 
 BUILD_BROKEN_DUP_RULES := true
 
@@ -21,7 +22,7 @@ MALLOC_SVELTE_FOR_LIBC32 := true
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG += xiaomi/pyxis.config
+TARGET_KERNEL_CONFIG += xiaomi/$(PRODUCT_HARDWARE).config
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
@@ -29,6 +30,7 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor-$(PRODUCT_HARDWARE).prop
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
