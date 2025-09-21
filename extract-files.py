e4a7d52e45329b285d276d7b08f1986f2ee504ed
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-09-21 16:31:05 +0200

    pyxis: Fix qcom hardware Soong namespace
    Change-Id: I795fa08a139b44b56779fb45b8e6fb770ee05f04

--- extract-files.py
+++ extract-files.py
@@ -21,7 +21,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'hardware/qcom/sdm845',
+    'hardware/qcom-caf/sdm845',
     'hardware/xiaomi',
     'vendor/xiaomi/sdm710-common',
 ]
